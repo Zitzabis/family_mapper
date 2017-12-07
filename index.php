6f27b14ea3b4cf8fa4586a7965318a62d4f9f097
Stephen Floyd <zitzabis.zitz@gmail.com>
2017-12-07 14:59:33 -0500

    Index + CSS: Basic layout.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <link rel="stylesheet" href="css/custom.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+
+    <script src="js/granim.min.js"></script>
+    
+
+    <body>
+        <!-- Create a <canvas> element -->
+        <div class="pageContent">
+            <h1>Hello World!</h1>
+        </div>
+        <canvas id="granim-canvas" class="backgroundCanvas"></canvas>
+        
+    </body>
+
+    <!-- Create a Granim instance -->
+    <script>
+        var granimInstance = new Granim({
+        element: '#granim-canvas',
+        name: 'granim',
+        opacity: [1, 1],
+        states : {
+            "default-state": {
+                gradients: [
+                    ['#834D9B', '#D04ED6'],
+                    ['#1CD8D2', '#93EDC7']
+                ]
+            }
+        }
+        });
+    </script>
+</html>
