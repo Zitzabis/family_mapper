7176ee4280db17bf92311e7d8c8a858f763daf31
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-12-09 01:16:56 -0500

    Head: Sepperates head content from the pages.

---
+++ components/head.php
@@ -0,0 +1,6 @@
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
