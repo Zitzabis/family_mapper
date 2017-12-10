0bc0899da7519992ec4e660b10fadb7b6afb4913
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-12-10 01:01:07 -0500

    PHP: Breaks page down into more partial PHP files.

--- components/head.php
+++ components/head.php
@@ -2,6 +2,6 @@
     <title>Page Title</title>
     <link rel="stylesheet" href="css/custom.css">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/animsition.css">
+    <link rel="stylesheet" href="css/sidebar.css">
     <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
 </head>
--- template.php
+++ template.php
@@ -4,14 +4,18 @@
         include_once("components/head.php");
         include_once("components/scripts.php");
     ?>
-
     <body>
-        <!-- Create a <canvas> element -->
-        <div class="pageContent centerContent"> 
-            <div>
-                <!-- Content Here -->
+        <?php include_once("components/navBarIcon.php"); ?>
+        <main>
+            <div class="pageContent centerContent"> 
+                <?php include_once("components/nav.php"); ?>
+                <div>
+                    <!-- Content Here -->
+                </div>
             </div>
-        </div>
+            <?php include_once("components/granim.php"); ?>
+        </main>
+        <?php include_once("components/sidebar.php"); ?>
     </body>
 
     <?php include_once("components/granim.php"); ?>
