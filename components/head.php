c56c339c9d0d3a7cf34c3c8f1e39069d4666aa5e
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-12-09 20:51:01 -0500

    Head: Reduces load time until live server

--- components/head.php
+++ components/head.php
@@ -2,5 +2,5 @@
     <title>Page Title</title>
     <link rel="stylesheet" href="css/custom.css">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
 </head>
