1dff3ab2d672a42d00e485c1b0b2cb2d9cb22214
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-12-09 23:26:38 -0500

    Template: Creates template file.

---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <?php
+        include_once("components/head.php");
+        include_once("components/scripts.php");
+    ?>
+
+    <body>
+        <!-- Create a <canvas> element -->
+        <div class="pageContent centerContent"> 
+            <div>
+                <!-- Content Here -->
+            </div>
+        </div>
+    </body>
+
+    <?php include_once("components/granim.php"); ?>
+</html>
