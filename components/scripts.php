ed5dfc638aa11fa139896326539e03f099f44c0d
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2017-12-09 20:51:40 -0500

    JS: Adds JQuery, Popper, Mo

--- components/scripts.php
+++ components/scripts.php
@@ -1,3 +1,5 @@
+<script src="js/popper.js"></script>
+<script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/granim.min.js"></script>
 <script src="js/anime.js"></script>
 <script src="js/bootstrap.js"></script>
